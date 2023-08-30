b055a1e16179c5e067b47b137586e781d88bce2c
KauanToldo <kauanlucastoldo@gmail.com>
2023-08-30 08:26:34 -0300

    Add coisas

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,13 @@ function converter() {
         resultado.innerHTML = 'Erro ao converter a criptomoeda.';
       });
   }
+
+  window.onscroll = () => {
+    document.querySelectorAll('.reveal').forEach(e => {
+      if (e.getBoundingClientRect().top < window.innerHeight - 150) {
+        e.classList.add('active');
+      } else {
+        e.classList.remove('active');
+      }
+    })
+  }
